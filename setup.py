e0509928b8b294a9ca6a3318fa75cfc1fb5478aa
Marc Daniel Ortiz <marcdanie.ortiz@gmail.com>
2025-07-30 15:32:21 +0800

    added exception, logger and setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "spaceship_titanic",
+    version = "0.0.0",
+    author = "marcdanielortiz",
+    author_email = "marcdaniel.ortiz@gmail.com",
+    packages = find_packages()
+)
